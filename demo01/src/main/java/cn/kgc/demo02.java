36d12dff4e355b40f95e60934753cdf5f7a95af5
whx <2983664989@qq.com>
2020-09-19 09:41:41 +0800

    测试一个demo02

---
+++ demo01/src/main/java/cn/kgc/demo02.java
@@ -0,0 +1,11 @@
+package cn.kgc;
+
+public class demo02 {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+}
