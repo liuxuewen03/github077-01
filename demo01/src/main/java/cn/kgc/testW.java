b4753c28797e7e654deccaaaa967e8ffa1e3e458
whx <2983664989@qq.com>
2020-09-19 09:28:24 +0800

    创建了一个testW类

---
+++ demo01/src/main/java/cn/kgc/testW.java
@@ -0,0 +1,25 @@
+package cn.kgc;
+
+import javax.swing.*;
+
+public class testW {
+    //王海兴创建了一个类
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
